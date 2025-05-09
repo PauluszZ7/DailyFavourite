ff3a9c441f515ddcbcb8f6c2091fb4c3d0073f22
PauluszZ7 <paul.koch2015@gmail.com>
2025-05-09 12:21:29 +0200

    fix

--- app/DailyFavourite/urls.py
+++ app/DailyFavourite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mainapp.views import *
+from mainapp.views import mainPage_view, loginPage_view, registrationPage_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
